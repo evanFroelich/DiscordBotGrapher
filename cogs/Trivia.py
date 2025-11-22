3647d868b7615417d69f58d833db479876eb5c9f
evanFroelich <evanfroe1234@yahoo.com>
2025-11-22 06:07:23 -0800

    Update gambling fund limit in Trivia to a maximum of 1000 and adjust validation message accordingly

--- cogs/Trivia.py
+++ cogs/Trivia.py
@@ -623,9 +623,9 @@ class GamblingIntroModal(discord.ui.Modal):
     async def on_submit(self, interaction: discord.Interaction):
         fundsInput = self.funds_input.value
         view=discord.ui.View()
-        if not fundsInput.isdigit() or int(fundsInput) > self.funds or int(fundsInput)<10 or int(fundsInput)>500:
+        if not fundsInput.isdigit() or int(fundsInput) > self.funds or int(fundsInput)<10 or int(fundsInput)>1000:
             view.add_item(GamblingButton(label="want to try that again?", user_id=self.user_id, guild_id=self.guild_id, style=discord.ButtonStyle.primary))
-            await interaction.response.send_message(f"*I cant bring that amount.*\n(cannot bring more than you have or less than 10 or more than 500)", ephemeral=True, view=view)
+            await interaction.response.send_message(f"*I cant bring that amount.*\n(cannot bring more than you have or less than 10 or more than 1000)", ephemeral=True, view=view)
             return
         
         gamesDB = "games.db"
